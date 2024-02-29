c404806137e7976f546234c190abb6023d95b282
vital250581 <vital2505@mail.ru>
2024-02-29 22:33:46 +1000

    Add workflow with PEP8 linting

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',    # nopep8
+            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
